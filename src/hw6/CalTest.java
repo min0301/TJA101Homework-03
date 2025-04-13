f64f587b2e83462f2028e618ade7c7342a8ecaed
jimmy <min0301.tw@gmail.com>
2025-04-13 22:58:11 +0800

    Hw6更新

--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -1,26 +1,73 @@
 package hw6;
 
+import java.security.DrbgParameters.NextBytes;
 import java.util.Scanner;
 
 public class CalTest {
+//	public static void main(String[] args) {
+//		
+//		
+//		System.out.println("===X的Y次方計算機===");
+//		Calculator xy = new Calculator();
+//		
+//		
+//		int x,y;
+//		try(Scanner sc = new Scanner(System.in)){
+//			System.out.print("請輸入X的值：");
+//			x = sc.nextInt();
+//			System.out.print("請輸入Y的值：");
+//			y = sc.nextInt();
+//			xy.powerXY(x,y);
+//	
+//		}catch (Exception e) {
+//			System.out.println("輸入格式不正確");
+//		}
+//	}
+	
+	
+	
 	public static void main(String[] args) {
 		
-		
-		System.out.println("===X的Y次方計算機===");
+
 		Calculator xy = new Calculator();
 		
-		
-		int x,y;
-		try(Scanner sc = new Scanner(System.in)){
-			System.out.print("請輸入X的值：");
-			x = sc.nextInt();
-			System.out.print("請輸入Y的值：");
-			y = sc.nextInt();
+		try(Scanner sc = new Scanner(System.in)){	
+			
+			int x , y;
+			
+			do {
+				try{
+					System.out.print("請輸入X的值：");
+					x = sc.nextInt();
+					break;
+
+				}catch (Exception e) {
+					System.out.println("輸入格式不正確");
+					sc.nextLine();
+				}
+			} while (true);
+			
+			
+			do {		
+				try{
+					System.out.print("請輸入Y的值：");
+					y = sc.nextInt();
+					if(y == x && y == 0) {
+						throw new CalException("0 的 0 次方沒有意義！");
+					}else if(y < 0) {
+						throw new CalException("次方為負數，回傳結果不為整數！");
+					}
+					break;
+					
+				}catch (Exception e) {
+					System.out.println(e);
+					sc.nextLine();
+				}
+			} while (true);
+			
 			xy.powerXY(x,y);
-	
-		}catch (Exception e) {
-			System.out.println("輸入格式不正確");
 		}
+	
 	}
 
 }
